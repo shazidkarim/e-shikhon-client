8dc7b83400010e00baee2adf60106bb7baa561b6
shazidkarim <shazid747@gmail.com>
2024-02-21 01:06:54 +0600

    dark mode added in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,11 @@
 'use client'
+import Link from 'next/link';
 import React,{FC, useState} from 'react'
 
 type Props = {
     open: boolean;
   setOpen: (open: boolean) => void;
   activeItem: number;
-//   route: string;
-//   setRoute: (route: string) => void;
 }
 
 const Header: FC<Props> =(props: Props) => {
@@ -29,7 +28,20 @@ const Header: FC<Props> =(props: Props) => {
             ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
             : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
         }`}
-      ></div>
+      >
+         <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+          <div className="w-full h-[80px] flex items-center justify-between p-3">
+            <div>
+              <Link
+                href={"/"}
+                className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              >
+                eShikhon
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
