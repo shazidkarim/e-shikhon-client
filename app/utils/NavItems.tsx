336c943c7a42aff7216955ccf732749ab04eb90d
shazidkarim <shazid747@gmail.com>
2024-02-21 17:38:01 +0600

    nav updated

--- app/utils/NavItems.tsx
+++ app/utils/NavItems.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 export const navItemsData = [
@@ -22,15 +23,52 @@ export const navItemsData = [
       url: "/faq",
     },
   ];
-  
+
 type Props = {
     activeItem:number;
     isMobile: boolean;
 }
 
-const NavItems = (props: Props) => {
+const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
   return (
-    <div>NavItems</div>
+    <>
+     <div className="hidden 800px:flex">
+        {navItemsData &&
+          navItemsData.map((i, index) => (
+            <Link href={`${i.url}`} key={index} passHref>
+              <span
+                className={`${
+                  activeItem === index
+                    ? "dark:text-[#37a39a] text-[crimson]"
+                    : "dark:text-white text-black"
+                } text-[18px] px-6 font-Poppins font-[400]`}
+              >
+                {i.name}
+              </span>
+            </Link>
+          ))}
+      </div>
+      {isMobile && (
+        <div className="800px:hidden mt-5">
+          <div className="w-full text-center py-6">
+            {
+                navItemsData && navItemsData.map((i,index)=>(
+                    <Link key={index} href="/" passHref>
+              <span 
+              className={`${
+                activeItem === index
+                  ? "dark:text-[#37a39a] text-[crimson]"
+                  : "dark:text-white text-black"
+              } text-[18px] px-6 font-Poppins font-[400]`}
+              >
+              </span>
+            </Link>
+            ))
+            }
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
