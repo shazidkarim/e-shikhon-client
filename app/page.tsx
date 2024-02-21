2759d6ab0bc1811112a1c36080177adbb22d5231
shazidkarim <shazid747@gmail.com>
2024-02-22 00:45:54 +0600

    hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
+import Hero from "./components/Route/Hero"
 interface Props {}
 
 const Page: FC<Props> = (props) => {
@@ -21,7 +22,7 @@ const Page: FC<Props> = (props) => {
       setOpen={setOpen}
       activeItem={activeItem}
       />
-      <h1>hello world</h1>
+      <Hero/>
     </div>
   );
 };
