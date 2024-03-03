3c7334de93c3b359a26e765612f25f3ef18988f9
shazidkarim <shazid747@gmail.com>
2024-03-03 22:34:54 +0600

    course data added

--- app/component/Admin/Course/CourseInformation.tsx
+++ app/component/Admin/Course/CourseInformation.tsx
@@ -1,11 +1,248 @@
-import React from 'react'
+import { styles } from "@/app/styles/style";
+import React, { FC, useEffect, useState } from "react";
 
-type Props = {}
+type Props = {
+  courseInfo: any;
+  setCourseInfo: (courseInfo: any) => void;
+  active: number;
+  setActive: (active: number) => void;
+};
 
-const CourseInformation = (props: Props) => {
+const CourseInformation: FC<Props> = ({
+  courseInfo,
+  setCourseInfo,
+  active,
+  setActive,
+}) => {
+  const [dragging, setDragging] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setActive(active + 1);
+  };
+
+  const handleFileChange = (e: any) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        if (reader.readyState === 2) {
+          setCourseInfo({ ...courseInfo, thumbnail: reader.result });
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDragOver = (e: any) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e: any) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+    setDragging(false);
+    const file = e.dataTransfer.file?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setCourseInfo({ ...courseInfo, thumbnail: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>CourseInformation</div>
-  )
-}
+    <div className="w-[80%] m-auto mt-24">
+      <form onSubmit={handleSubmit} className={`${styles.label}`}>
+        <div>
+          <label htmlFor="">Course Name</label>
+          <input
+            type="name"
+            name=""
+            id="name"
+            required
+            value={courseInfo.name}
+            onChange={(e: any) =>
+              setCourseInfo({ ...courseInfo, name: e.target.value })
+            }
+            placeholder="MERN stack LMS Platform with next js"
+            className={`${styles.input}`}
+          />
+        </div>
+        <br />
+        <div className="mb-5">
+          <label htmlFor="">Course Description</label>
+          <textarea
+            name=""
+            id=""
+            cols={30}
+            rows={8}
+            placeholder="Write something amazing..."
+            className={`${styles.input} !h-min py-2`}
+            value={courseInfo.description}
+            onChange={(e: any) =>
+              setCourseInfo({ ...courseInfo, description: e.target.value })
+            }
+          ></textarea>
+        </div>
+        <br />
+        <div className="w-full flex justify-between">
+          <div className="w-[45%]">
+            <label htmlFor="">Course Price</label>
+            <input
+              type="number"
+              name=""
+              id="price"
+              required
+              value={courseInfo.price}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, price: e.target.value })
+              }
+              placeholder="29"
+              className={`${styles.input}`}
+            />
+          </div>
+          <div className="w-[45%]">
+            <label htmlFor="">Estimated Price (optional)</label>
+            <input
+              type="number"
+              name=""
+              id="price"
+              required
+              value={courseInfo.estimatedPrice}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, estimatedPrice: e.target.value })
+              }
+              placeholder="29"
+              className={`${styles.input}`}
+            />
+          </div>
+        </div>
+        <br />
+        <div className="w-full flex justify-between">
+          <div className="w-[45%]">
+            <label htmlFor="email" className={`${styles.label}`}>
+              Course Tags
+            </label>
+            <input
+              type="text"
+              name=""
+              id="tags"
+              required
+              value={courseInfo.tags}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, tags: e.target.value })
+              }
+              placeholder="MERN,NEXT 13"
+              className={`${styles.input}`}
+            />
+          </div>
+          <div className="w-[45%]">
+            <label htmlFor="">Course Categories</label>
+            <select
+              name=""
+              id=""
+              className={`${styles.input}`}
+              value={courseInfo.category}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, category: e.target.value })
+              }
+            >
+              <option value="">Select Category</option>
+              {categories.map((item: any) => (
+                <option value={item.id} key={item.id}>
+                  {item.title}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <br />
+        <div className="w-full flex justify-between">
+          <div className="w-[45%]">
+            <label htmlFor="">Course Label</label>
+            <input
+              type="text"
+              name=""
+              id="lavel"
+              required
+              value={courseInfo.level}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, level: e.target.value })
+              }
+              placeholder="Biginner/Expert"
+              className={`${styles.input}`}
+            />
+          </div>
+          <div className="w-[45%]">
+            <label htmlFor="">Demo Url</label>
+            <input
+              type="text"
+              name=""
+              id="demoUrl"
+              required
+              value={courseInfo.demoUrl}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, demoUrl: e.target.value })
+              }
+              placeholder="eer74fd"
+              className={`${styles.input}`}
+            />
+          </div>
+        </div>
+        <br />
+        <div className="w-full">
+          <input
+            type="file"
+            accept="image/*"
+            id="file"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+          <label
+            htmlFor="file"
+            className={`w-full min-h-[10vh] dark:border-white border-[#00000026] p-3 border flex items-center justify-center ${
+              dragging ? "bg-blue-500" : "bg-transparent"
+            } `}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+          >
+            {courseInfo.thumbnail ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={courseInfo.thumbnail}
+                alt=""
+                className="max-h-full w-full object-cover"
+              />
+            ) : (
+              <span className="text-black dark:text-white">
+                Drag and drop your thumbnail here or click to browse
+              </span>
+            )}
+          </label>
+        </div>
+        <br />
+        <div>
+          <input
+            type="submit"
+            value="Next"
+            className="w-full 800px:w-[180px] h-[40px] bg-[#37a39a] text-center text-white rounded mt-8 cursor-pointer "
+          />
+        </div>
+        <br />
+        <br />
+      </form>
+    </div>
+  );
+};
 
-export default CourseInformation
+export default CourseInformation;
--- app/component/Admin/Course/CreateCourse.tsx
+++ app/component/Admin/Course/CreateCourse.tsx
@@ -1,11 +1,158 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import CourseInformation from "./CourseInformation";
+import CourseOptions from "./CourseOptions";
+import CourseData from "./CourseData";
+import CourseContent from "./CourseContent";
+import CoursePreview from "./CoursePreview";
+import { useCreateCourseMutation } from "../../../../redux/features/courses/coursesApi";
+import toast from "react-hot-toast";
+import { redirect } from "next/navigation";
 
-type Props = {}
+type Props = {};
 
 const CreateCourse = (props: Props) => {
-  return (
-    <div>CreateCourse</div>
-  )
-}
+  const [createCourse, { isLoading, isSuccess, error }] =
+    useCreateCourseMutation();
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Course create Successfully");
+      redirect("/admin/courses");
+    }
+    if (error) {
+      if ("data" in error) {
+        const errorMessage = error as any;
+        toast.error(errorMessage.data.message);
+      }
+    }
+  }, [isSuccess, isLoading, error]);
+  const [active, setActive] = useState(0);
+  const [courseInfo, setCourseInfo] = useState({
+    name: "",
+    description: "",
+    price: "",
+    estimatedPrice: "",
+    tags: "",
+    level: "",
+    categories: "",
+    demoUrl: "",
+    thumbnail: "",
+  });
+  const [benefits, setBenefits] = useState([{ title: "" }]);
+  const [prerequisites, setPrerequisites] = useState([{ title: "" }]);
+  const [courseContentData, setCourseContentData] = useState([
+    {
+      videoUrl: "",
+      title: "",
+      description: "",
+      videoSection: "Untitled Section",
+      videoLength: "",
+      links: [
+        {
+          title: "",
+          url: "",
+        },
+      ],
+      suggestion: "",
+    },
+  ]);
+  const [courseData, setCourseData] = useState({});
 
-export default CreateCourse
+  const handleSubmit = () => {
+    // Formet benefits array
+    const formettedBenefits = benefits.map((benefit) => ({
+      title: benefit.title,
+    }));
+    const formattedPrerquisites = prerequisites.map((prerequisite) => ({
+      title: prerequisite.title,
+    }));
+
+    // Frome course contant array
+    const formattedCourseContentData = courseContentData.map(
+      (courseContent) => ({
+        videoUrl: courseContent.videoUrl,
+        title: courseContent.title,
+        description: courseContent.description,
+        videoLength: courseContent.videoLength,
+        videoSection: courseContent.videoSection,
+        links: courseContent.links.map((link) => ({
+          title: link.title,
+          url: link.url,
+        })),
+        suggestion: courseContent.suggestion,
+      })
+    );
+    // prepare our data object
+    const data = {
+      name: courseInfo.name,
+      description: courseInfo.description,
+      price: courseInfo.price,
+      estimatedPrice: courseInfo.estimatedPrice,
+      tags: courseInfo.tags,
+      thumbnail: courseInfo.thumbnail,
+      level: courseInfo.level,
+      demoUrl: courseInfo.demoUrl,
+      totalVides: CourseContent.length,
+      benefits: formettedBenefits,
+      prerequisites: formattedPrerquisites,
+      courseData: formattedCourseContentData,
+    };
+
+    setCourseData(data);
+  };
+
+  const hanleCourseCreate = async (e: any) => {
+    const data = courseData;
+
+    if (!isLoading) {
+      await createCourse(data);
+    }
+  };
+
+  return (
+    <div className="w-full flex min-h-screen">
+      <div className="w-[80%]">
+        {active === 0 && (
+          <CourseInformation
+            courseInfo={courseInfo}
+            setCourseInfo={setCourseInfo}
+            active={active}
+            setActive={setActive}
+          />
+        )}
+        {active === 1 && (
+          <CourseData
+            benefits={benefits}
+            setBenefits={setBenefits}
+            prerequisites={prerequisites}
+            setPrerequisites={setPrerequisites}
+            active={active}
+            setActive={setActive}
+          />
+        )}
+        {active === 2 && (
+          <CourseContent
+            active={active}
+            setActive={setActive}
+            courseContentData={courseContentData}
+            setCourseContentData={setCourseContentData}
+            handleSubmit={handleSubmit}
+          />
+        )}
+        {active === 3 && (
+          <CoursePreview
+            active={active}
+            setActive={setActive}
+            courseData={courseData}
+            hanleCourseCreate={hanleCourseCreate}
+          />
+        )}
+      </div>
+      <div className="w-[20%] mt-[100px] h-screen fixed z-[-1] top-8 right-0 ">
+        <CourseOptions active={active} setActive={setActive} />
+      </div>
+    </div>
+  );
+};
+
+export default CreateCourse;
