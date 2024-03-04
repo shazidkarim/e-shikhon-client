d1aada30fd2653d2e93d6af0457cce58e0179928
shazidkarim <shazid747@gmail.com>
2024-03-04 23:00:00 +0600

    course player added

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
 
 type Props = {
-    videoUrl: string;
-    title: string;
-  };
+  videoUrl: string;
+  title: string;
+};
 
-const CoursePlayer = (props: Props) => {
+const CoursePlayer: FC<Props> = ({ videoUrl }) => {
+  const [videoData, setVidoData] = useState({
+    otp: "",
+    playbackInfo: "",
+  });
+
+  useEffect(() => {
+    axios.post(`${process.env.NEXT_PUblIC_API_URL}getVdoCipherOTP`, {
+        videoId: videoUrl,
+      })
+      .then((res) => {
+        setVidoData(res.data);
+      });
+  }, [videoUrl]);
+
   return (
-    <div>CoursePlayer</div>
-  )
-}
+    <div style={{ padding: "41%|", position: "relative" }}>
+      {videoData.otp && videoData.playbackInfo !== "" && (
+        <iframe
+          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=Ub90iZXIOeUXH0Nv`}
+          style={{
+            border: "0",
+            width: "90%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          allowFullScreen={true}
+          allow="encrypted-media"
+        ></iframe>
+      )}
+    </div>
+  );
+};
 
-export default CoursePlayer
+export default CoursePlayer;
