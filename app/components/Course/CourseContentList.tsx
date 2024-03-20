bc7a1894f04d19cae6332b356419fef62690f5f3
shazidkarim <shazid747@gmail.com>
2024-03-20 15:05:49 +0600

    content list updated

--- app/components/Course/CourseContentList.tsx
+++ app/components/Course/CourseContentList.tsx
@@ -1,22 +1,81 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState } from "react";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 type Props = {
-    data:any;
-    activeVideo?: number;
-    setActiveVideo?:any;
-}
+  data: any;
+  activeVideo?: number;
+  setActiveVideo?: any;
+  isDemo?: boolean;
+};
 
 const CourseContentList: FC<Props> = (props) => {
-    const [visibleSections, setVisibleSections] = useState<Set<string>> (
-        new Set<string>()
-    );
-    // find unique video section 
-const videoSections : string[] = [
-    ...new Set<string>(props.data?.map((item:any) => item.videoSection))
-];
-  return (
-    <div>CourseContentList</div>
-  )
-}
+  const [visibleSections, setVisibleSections] = useState<Set<string>>(
+    new Set<string>()
+  );
+  // find unique video section
+  const videoSections: string[] = [
+    ...new Set<string>(props.data?.map((item: any) => item.videoSection)),
+  ];
 
-export default CourseContentList
+  let totalCount: number = 0;
+
+  const toggleSection = (section: string) => {
+    const newVisibleSections = new Set(visibleSections);
+    if (newVisibleSections.has(section)) {
+      newVisibleSections.delete(section);
+    } else {
+      newVisibleSections.add(section);
+    }
+    setVisibleSections(newVisibleSections);
+  };
+  return (
+    <div
+      className={`mt-[15px] w-full ${
+        !props.isDemo && "ml-[-30px] min-h-screen sticky top-24 left-0 z-30"
+      }`}
+    >
+      {videoSections.map((section: string, sectionIndex: number) => {
+        const isSectionVisible = visibleSections.has(section);
+        const sectionVideos: any[] = props.data.filter(
+          (item: any) => item.videSection === section
+        );
+        const sectionVideoCount: number = sectionVideos.length;
+        const sectionVideoLength: number = sectionVideos.reduce(
+          (totalLength: number, item: any) => totalLength + item.videoLength,
+          0
+        );
+        const sectionStartIndex: number = totalCount;
+        totalCount += sectionVideoCount;
+        const sectionCountHours: number = sectionVideoLength / 60;
+        return (
+          <div
+            className={`${!props.isDemo && "border-b border-[#ffffff8e] pb-2"}`}
+            key={section}
+          >
+            <div className="w-full flex">
+              {/* render video section */}
+              <div className="w-full flex justify-between items-center">
+                <h2 className="text-[22px] text-black dark:text-white">
+                  {" "}
+                  {section}{" "}
+                </h2>
+                <button
+                  className="mr-4 cursor-pointer text-black dark:text-white"
+                  onClick={() => toggleSection(section)}
+                >
+                  {isSectionVisible ? (
+                    <BsChevronUp size={20} />
+                  ) : (
+                    <BsChevronDown size={20} />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CourseContentList;
