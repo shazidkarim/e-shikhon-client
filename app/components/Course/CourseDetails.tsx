2c2585538a9220571248b9afe0551850780c8cae
shazidkarim <shazid747@gmail.com>
2024-03-21 14:52:55 +0600

    checkout form created

--- app/components/Course/CourseDetails.tsx
+++ app/components/Course/CourseDetails.tsx
@@ -7,11 +7,15 @@ import { useSelector } from "react-redux";
 import { format } from "timeago.js";
 import { styles } from "../../../app/styles/style";
 import CourseContentList from "../Course/CourseContentList";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from '../Payment/CheckOutForm';
 type Props = {
   data: any;
+  clientSecret:string;
+  stripePromise:any;
 };
 
-const CourseDetails = ({ data }: Props) => {
+const CourseDetails = ({ data,clientSecret,stripePromise }: Props) => {
   const { user } = useSelector((state: any) => state.auth);
   const [open, setOpen] = useState(false);
   const discountPercentenge =
@@ -207,11 +211,22 @@ const CourseDetails = ({ data }: Props) => {
         {open && (
           <div className="w-full h-screen bg-[#00000036] fixed top-0 left-0 z-50 flex items-center justify-center ">
             <div className="w-[500px] min-h-[500px] bg-white rounded-xl shadow p-3">
+              <div className="w-full flex justify-end">
               <IoCloseOutline
                 size={40}
                 className="text-black cursor-pointer"
                 onClick={() => setOpen(false)}
               />
+              </div>
+              <div className="w-full">
+                {
+                  stripePromise && clientSecret &&(
+                    <Elements stripe={stripePromise} options={{clientSecret}}>
+                      <CheckOutForm setOpen={setOpen} data={data}/>
+                    </Elements>
+                  )
+                }
+              </div>
             </div>
           </div>
         )}
--- app/components/Course/CourseDetailsPage.tsx
+++ app/components/Course/CourseDetailsPage.tsx
@@ -1,12 +1,13 @@
 import Heading from "@/app/utils/Heading";
 import Loader from "../../../app/component/Loader";
 import { useGetCourseDetailsQuery } from "../../../redux/features/courses/coursesApi";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataArray } from "@mui/icons-material";
 import Header from "../Header";
 import Footer from "../Footer";
 import CourseDetails from "./CourseDetails";
-import { useGetStripePublishablekeyQuery } from "../../../redux/features/orders/OrdersApi";
+import { useCreatePaymentIntentMutation, useGetStripePublishablekeyQuery } from "../../../redux/features/orders/OrdersApi";
+import { loadStripe } from "@stripe/stripe-js";
 
 type Props = {
   id: string;
@@ -15,8 +16,38 @@ type Props = {
 const CourseDetailsPage = ({ id }: Props) => {
   const [route, setRoute] = useState("login");
   const [open, setOpen] = useState(false);
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [ClientSecret, setClientSecret] = useState('');
 const {data,isLoading} = useGetCourseDetailsQuery(id);
 const {data:config} = useGetStripePublishablekeyQuery({});
+const [createPaymentIntent, {data:paymentIntentData}] = useCreatePaymentIntentMutation();
+
+
+useEffect(() => {
+  if(config){
+    const publishablekey = config?.publishableKey;
+    setStripePromise(loadStripe(publishablekey));
+  }
+if(data){
+  const amount = Math.round(data.course.price * 100);
+  createPaymentIntent(amount);
+}
+ 
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [config, data]);
+
+
+useEffect(()=>{
+  if(paymentIntentData){
+    setClientSecret(paymentIntentData?.client_secret);
+  }
+}, [paymentIntentData]);
+
+
+
+
+
+
   return (
     <>
       {isLoading ? (
@@ -37,7 +68,11 @@ const {data:config} = useGetStripePublishablekeyQuery({});
             setRoute={setRoute}
             route={route}
           />
-          <CourseDetails data={data.course} />
+          {
+            stripePromise && (
+              <CourseDetails data={data.course}  stripePromise={stripePromise} clientSecret={ClientSecret}/>
+            )
+          }
           <Footer />
         </div>
       )}
